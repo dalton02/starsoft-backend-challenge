c67a744c97108f279a023a08b2cafbf0d42d41a6
daltonPinaculo <dalton@pinaculodigital.com.br>
2026-02-09 21:22:40 -0300

    fix -> removing handler of prisma errors

--- src/utils/errors/errors.filter.ts
+++ src/utils/errors/errors.filter.ts
@@ -8,11 +8,7 @@ import {
 } from '@nestjs/common';
 
 import { AppError } from './app-errors';
-import {
-  PrismaClientKnownRequestError,
-  PrismaClientInitializationError,
-  PrismaClientRustPanicError,
-} from '@prisma/client/runtime/client';
+import { QueryFailedError } from 'typeorm';
 
 @Catch(Error)
 export class AppErrorFilter implements ExceptionFilter {
@@ -37,98 +33,7 @@ export class AppErrorFilter implements ExceptionFilter {
       message = httpError.message;
     }
 
-    // Tratamento de erros de validação
-    // vefirica se a exceção possui uma response com a propriedade message
-    /*else if ( exception.response && exception.response.message ) {
-      status = exception.status;
-      message = exception.message
-      // Se a mensagem for um array de erros de validação
-      if (Array.isArray(exception.response.message) && exception.response.message.length > 0) {
-        message = `${message}: `;
-        // Concatena todos os erros em uma única string
-        exception.response.message.map((error: string) => {
-          message += error + '; ' ;
-        })
-        // remove o último '; '
-        message = message.slice(0, -2);
-      } 
-    }*/
-
-    // Tratamento de erros do Prisma
-    if (exception instanceof PrismaClientKnownRequestError) {
-      switch (exception.code) {
-        case 'P2002':
-          status = HttpStatus.CONFLICT;
-          message = `Conflito de dados detectado para o registro ${exception.meta.modelName} no campo ${exception.meta.target}.`;
-          break;
-        case 'P2003':
-          status = HttpStatus.BAD_REQUEST;
-          message = `Violação de chave estrangeira no registro ${exception.meta.modelName}.`;
-          break;
-        case 'P2004':
-          status = HttpStatus.BAD_REQUEST;
-          message = `Falha de restrição no modelo ${exception.meta.modelName}.`;
-          break;
-        case 'P2005':
-          status = HttpStatus.BAD_REQUEST;
-          message = `O valor armazenado na coluna não é válido para o tipo de coluna.`;
-          break;
-        case 'P2006':
-          status = HttpStatus.BAD_REQUEST;
-          message = `O valor fornecido para a coluna é inválido.`;
-          break;
-        case 'P2007':
-          status = HttpStatus.BAD_REQUEST;
-          message = `Os dados do registro estão em um formato inválido.`;
-          break;
-        case 'P2008':
-          status = HttpStatus.INTERNAL_SERVER_ERROR;
-          message = `Erro ao validar a consulta Prisma.`;
-          break;
-        case 'P2009':
-          status = HttpStatus.INTERNAL_SERVER_ERROR;
-          message = `Erro ao validar uma variável na consulta Prisma.`;
-          break;
-        case 'P2010':
-          status = HttpStatus.UNAUTHORIZED;
-          message = `Falha na autenticação.`;
-          break;
-        case 'P2011':
-          status = HttpStatus.BAD_REQUEST;
-          message = `Violação de restrição de chave exclusiva.`;
-          break;
-        case 'P2012':
-          status = HttpStatus.BAD_REQUEST;
-          message = `Violação de restrição de chave primária.`;
-          break;
-        case 'P2013':
-          status = HttpStatus.BAD_REQUEST;
-          message = `Nenhum campo de argumento foi fornecido para a relação.`;
-          break;
-        case 'P2014':
-          status = HttpStatus.BAD_REQUEST;
-          message = `O número de campos de argumento não corresponde ao esperado.`;
-          break;
-        case 'P2021':
-          status = HttpStatus.INTERNAL_SERVER_ERROR;
-          message = `Tabela ${exception.meta.modelName} não encontrada no banco .`;
-          break;
-        case 'P2025':
-          status = HttpStatus.NOT_FOUND;
-          message = `Registro não encontrado ${exception.meta.table}.`;
-          break;
-        default:
-          status = HttpStatus.INTERNAL_SERVER_ERROR;
-          message = 'Erro interno do servidor.';
-          break;
-      }
-    } else if (exception instanceof PrismaClientInitializationError) {
-      status = HttpStatus.SERVICE_UNAVAILABLE;
-      message = 'Falha ao inicializar o cliente do banco de dados.';
-    } else if (exception instanceof PrismaClientRustPanicError) {
-      status = HttpStatus.INTERNAL_SERVER_ERROR;
-      message = 'Erro interno do cliente do banco de dados.';
-    } else if (exception.message.includes('Transaction failed')) {
+    if (exception.message.includes('Transaction failed')) {
       status = HttpStatus.INTERNAL_SERVER_ERROR;
       message = 'Falha na transação do banco de dados.';
     } else if (exception instanceof BadRequestException) {
